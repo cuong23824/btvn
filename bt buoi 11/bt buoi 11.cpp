c5cc91f16de4a830f5a81e75963f045ab03700e2
cuong23824 <cuong23o82oo4@gmail.com>
2022-10-24 09:50:42 +0700

    bt buoi 11

---
+++ bt buoi 11/bt buoi 11.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+	printf("1 \n");
+	for (int i=0;i<3;i++){
+		for (int j=0;j<5;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	printf("2 \n");
+		for (int i=0;i<5;i++){
+		for (int j=0;j<5;j++){
+			printf("*");
+			}
+			if(i%2==0){
+				printf("*");
+			}
+			printf("\n");
+		}
+		
+	printf("3 \n");
+			for (int i=0;i<5;i++){
+		for(int j=0;j<=i;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	printf("4 \n");
+	for (int i=0;i<5;i++){
+		for(int h=0;h<=i;h++){
+		for(int j=0;j<=i;j++){
+			printf("*");
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
+}
