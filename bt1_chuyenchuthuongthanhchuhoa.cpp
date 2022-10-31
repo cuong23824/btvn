eb10d81e5974e1ed78ecafd6ad7f8435676e43d7
cuong23824 <cuong23o82oo4@gmail.com>
2022-10-31 11:39:46 +0700

    bt_buoi18

---
+++ bt1_chuyenchuthuongthanhchuhoa.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+//void chuyenInHoa(char s[]){
+//	for (int i=0;i<strlen(s);i++){
+//		if(m[i]>96 && m[i]<123){
+//			m[i]=m[i]-32;
+//		}
+//	}
+//}
+int main(){
+	char m[50];
+	scanf("%s",m);
+	for (int i=0;i<strlen(s);i++){
+		if(m[i]>96 && m[i]<123){
+			m[i]=m[i]-32;
+		}
+	}
+	printf("%s",m);
+}
